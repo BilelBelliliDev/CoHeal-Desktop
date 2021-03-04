1d6f3557c5732d31ebfb301fc9aeb684a813d6dc
abassimarwen <47828392+abassimarwen@users.noreply.github.com>
2021-03-04 04:15:39 +0100

    crud BookCategory

---
+++ src/coheal/iservices/book/IserviceBookCategory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coheal.iservices.book;
+
+import coheal.entities.Book.BookCategory;
+import java.sql.SQLException;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Marwen
+ */
+public interface IserviceBookCategory {
+         public void AjouterBookCategory(BookCategory bc);
+         public ObservableList<BookCategory>AfficherBookCat()throws SQLException ;
+     public void supprimerBookCat(int id);
+     public void modifierBookCat(int id, BookCategory bc);
+}
