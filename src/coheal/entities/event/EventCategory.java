aad1a6be14c19395cb700a37183e6353f1fe397f
belaid10 <66521777+belaid10@users.noreply.github.com>
2021-03-04 13:37:02 +0100

    added event module crud

---
+++ src/coheal/entities/event/EventCategory.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coheal.entities.event;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author HP
+ */
+public class EventCategory {
+    private int catId;
+    private String name;
+    private String imgUrl;
+    private boolean isDeleted;
+    private Timestamp deletedAt;
+    private Timestamp created_At;
+    private Timestamp modified_at;
+
+    public int getCatId() {
+        return catId;
+    }
+
+    public void setCatId(int catId) {
+        this.catId = catId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public boolean isIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public Timestamp getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Timestamp created_At) {
+        this.created_At = created_At;
+    }
+
+    public Timestamp getModified_at() {
+        return modified_at;
+    }
+
+    public void setModified_at(Timestamp modified_at) {
+        this.modified_at = modified_at;
+    }
+    
+    
+}
