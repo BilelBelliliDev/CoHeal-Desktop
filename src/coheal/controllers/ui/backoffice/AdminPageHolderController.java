5d6dff3234996da95c622c53d546e4a336eafd33
BilelBelliliDev <73437985+BilelBelliliDev@users.noreply.github.com>
2021-03-28 11:52:06 +0100

    add table view css

--- src/coheal/controllers/ui/backoffice/AdminPageHolderController.java
+++ src/coheal/controllers/ui/backoffice/AdminPageHolderController.java
@@ -159,11 +159,11 @@ public class AdminPageHolderController implements Initializable {
         slide.setToY(481);
         slide.play();
         pageHolder.getChildren().removeAll(pageHolder.getChildren());
-//        try {
-//            pageHolder.getChildren().add(FXMLLoader.load(getClass().getResource("/coheal/views/ui/frontoffice/book/BookPage.fxml")));
-//        } catch (IOException ex) {
-//            Logger.getLogger(AdminPageHolderController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+            pageHolder.getChildren().add(FXMLLoader.load(getClass().getResource("/coheal/views/ui/backoffice/report/ModerationPage.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(AdminPageHolderController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
