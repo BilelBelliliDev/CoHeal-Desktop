bed22683fe7513ee152dc261ded99cb3ebec883b
BilelBelliliDev <73437985+BilelBelliliDev@users.noreply.github.com>
2021-04-02 09:17:34 +0100

    update

--- src/coheal/controllers/ui/frontoffice/report/RecentReportItemController.java
+++ src/coheal/controllers/ui/frontoffice/report/RecentReportItemController.java
@@ -5,13 +5,24 @@
  */
 package coheal.controllers.ui.frontoffice.report;
 
+import coheal.controllers.ui.frontoffice.HomePageHolderController;
 import coheal.entities.report.Report;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -24,6 +35,8 @@ public class RecentReportItemController implements Initializable {
     private Label label;
     @FXML
     private FontAwesomeIconView icon;
+    Report report = null;
+    double xOffset, yOffset;
 
     /**
      * Initializes the controller class.
@@ -31,29 +44,65 @@ public class RecentReportItemController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    public void setData(Report r){
+    }
+
+    public void setData(Report r) {
+        report = r;
         switch (r.getType()) {
             case "task":
                 icon.setGlyphName("TASKS");
-                label.setText("\""+r.getTitle()+"\" reported because of: \""+r.getNote()+"\"");
+                label.setText("\"" + r.getTitle() + "\" reported because of: \"" + r.getNote() + "\"");
                 break;
             case "book":
                 icon.setGlyphName("BOOK");
-                label.setText("\""+r.getTitle()+"\" reported because of: \""+r.getNote()+"\"");
+                label.setText("\"" + r.getTitle() + "\" reported because of: \"" + r.getNote() + "\"");
                 break;
             case "event":
                 icon.setGlyphName("CALENDAR");
-                label.setText("\""+r.getTitle()+"\" reported because of: \""+r.getNote()+"\"");
+                label.setText("\"" + r.getTitle() + "\" reported because of: \"" + r.getNote() + "\"");
                 break;
             case "session":
                 icon.setGlyphName("USER_MD");
-                label.setText("\""+r.getTitle()+"\" reported because of: \""+r.getNote()+"\"");
+                label.setText("\"" + r.getTitle() + "\" reported because of: \"" + r.getNote() + "\"");
                 break;
             case "recipe":
                 icon.setGlyphName("STICKY_NOTE");
-                label.setText("\""+r.getTitle()+"\" reported because of: \""+r.getNote()+"\"");
+                label.setText("\"" + r.getTitle() + "\" reported because of: \"" + r.getNote() + "\"");
                 break;
         }
     }
+
+    @FXML
+    private void details(MouseEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/coheal/views/ui/frontoffice/report/OpenReportDetailsPage.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(RecentReportItemController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        OpenReportDetailsPageController c = loader.getController();
+        c.setData(report);
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        HomePageHolderController hpc = new HomePageHolderController();
+        hpc.setStage(stage);
+        stage.show();
+        root.setOnMousePressed((MouseEvent mouseEvent) -> {
+            xOffset = mouseEvent.getSceneX();
+            yOffset = mouseEvent.getSceneY();
+        });
+        root.setOnMouseDragged((MouseEvent mouseEvent) -> {
+            stage.setX(mouseEvent.getScreenX() - xOffset);
+            stage.setY(mouseEvent.getScreenY() - yOffset);
+            stage.setOpacity(0.85f);
+        });
+        root.setOnMouseReleased((MouseEvent mouseEvent) -> {
+            stage.setOpacity(1.0f);
+        });
+    }
+
 }
--- src/coheal/controllers/ui/frontoffice/report/UrgentReportItemController.java
+++ src/coheal/controllers/ui/frontoffice/report/UrgentReportItemController.java
@@ -5,13 +5,24 @@
  */
 package coheal.controllers.ui.frontoffice.report;
 
+import coheal.controllers.ui.frontoffice.HomePageHolderController;
 import coheal.entities.report.Report;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -24,6 +35,8 @@ public class UrgentReportItemController implements Initializable {
     private FontAwesomeIconView icon;
     @FXML
     private Label text;
+    Report report = null;
+    double xOffset, yOffset;
 
     /**
      * Initializes the controller class.
@@ -31,29 +44,65 @@ public class UrgentReportItemController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    public void setData(Report r){
+    }
+
+    public void setData(Report r) {
+        report = r;
         switch (r.getType()) {
             case "task":
                 icon.setGlyphName("TASKS");
-                text.setText("\""+r.getTitle()+"\" reported "+r.getTotal()+" times");
+                text.setText("\"" + r.getTitle() + "\" reported " + r.getTotal() + " times");
                 break;
             case "book":
                 icon.setGlyphName("BOOK");
-                text.setText("\""+r.getTitle()+"\" reported "+r.getTotal()+" times");
+                text.setText("\"" + r.getTitle() + "\" reported " + r.getTotal() + " times");
                 break;
             case "event":
                 icon.setGlyphName("CALENDAR");
-                text.setText("\""+r.getTitle()+"\" reported "+r.getTotal()+" times");
+                text.setText("\"" + r.getTitle() + "\" reported " + r.getTotal() + " times");
                 break;
             case "session":
                 icon.setGlyphName("USER_MD");
-                text.setText("\""+r.getTitle()+"\" reported "+r.getTotal()+" times");
+                text.setText("\"" + r.getTitle() + "\" reported " + r.getTotal() + " times");
                 break;
             case "recipe":
                 icon.setGlyphName("STICKY_NOTE");
-                text.setText("\""+r.getTitle()+"\" reported "+r.getTotal()+" times");
+                text.setText("\"" + r.getTitle() + "\" reported " + r.getTotal() + " times");
                 break;
         }
     }
+
+    @FXML
+    private void details(MouseEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/coheal/views/ui/frontoffice/report/OpenReportDetailsPage.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(UrgentReportItemController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        OpenReportDetailsPageController c = loader.getController();
+        c.setData(report);
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        HomePageHolderController hpc = new HomePageHolderController();
+        hpc.setStage(stage);
+        stage.show();
+        root.setOnMousePressed((MouseEvent mouseEvent) -> {
+            xOffset = mouseEvent.getSceneX();
+            yOffset = mouseEvent.getSceneY();
+        });
+        root.setOnMouseDragged((MouseEvent mouseEvent) -> {
+            stage.setX(mouseEvent.getScreenX() - xOffset);
+            stage.setY(mouseEvent.getScreenY() - yOffset);
+            stage.setOpacity(0.85f);
+        });
+        root.setOnMouseReleased((MouseEvent mouseEvent) -> {
+            stage.setOpacity(1.0f);
+        });
+    }
+
 }
